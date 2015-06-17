87850a4c459c6a34940fa87cd1fe705bc92691a6
Junggon Kim <junggonkim@gmail.com>
2015-06-17 10:00:48 +0900

    initial

---
+++ jgkimLib.js
@@ -0,0 +1,6 @@
+var JGKIMLib = (function(JGKIMLib, $, undefined){
+    var 서브 객체 = function (_id, _value){ this.id = _id;
+        this.value = _value;
+    };
+    return JGKIMLib; // 리턴을 해야함
+})(window.JGKIMLib || {},jQuery); // 객체 없으면 생성 (namespace = window.namespace || {})
